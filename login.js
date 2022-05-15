ca282fe2657e46c58b54de1ee621359b7fe5f30e
ayush13073 <104487018+ayush13073@users.noreply.github.com>
2022-05-15 16:51:38 +0530

    Update login.js

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ var attempt = 3; // Variable to count number of attempts.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "priyapandeykiaara@gmail.com" && password == "password"){
+if ( username == "priyapandeykiaara@gmail.com" && password == "password" || username == "Formget" && password == "formget"){
 alert ("Login successfully");
 window.location = "welcome.html"; // Redirecting to other page.
 return false;
