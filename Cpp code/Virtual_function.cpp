28209f6d18fd34d10dbc2efde0b45ee6367eef3c
ayush13073 <104487018+ayush13073@users.noreply.github.com>
2022-10-12 08:22:58 +0530

    Create Virtual_function.cpp

---
+++ Cpp code/Virtual_function.cpp
@@ -0,0 +1,55 @@
+#include <iostream.h> 
+#include <conio.h> 
+#include <string.h> 
+ 
+class person 
+{ 
+    public: 
+        int age; 
+        char name2[20]; 
+        virtual void show(char name[], int a) 
+        { 
+            strcpy (name2, name); 
+            age = a; 
+            cout << "Name : " << name2 << endl; 
+            cout << "Age : " << age << endl; 
+        } 
+}; 
+ 
+class derive:public person 
+{ 
+    public: 
+        int age2; 
+        char name1[50]; 
+        void show (char name3[], int b) 
+        { 
+            strcpy (name1, name3); 
+            age2 = b; 
+            cout << "Name : " << name1 << endl; 
+            cout << "Age : " << age2 << endl; 
+        } 
+}; 
+
+void main () 
+{ 
+    clrscr(); 
+    person * p; 
+    derive d1; 
+    person p1; 
+    p = &p1; 
+    cout << "BASE CLASS : " << endl; 
+    p -> show ("ANONYMOUS" , 17); 
+    p = &d1; 
+    cout << "DERIVED CLASS : " << endl; 
+    p -> show ("ANGEL", 17); 
+    getch(); 
+} 
+/* 
+Output: 
+BASE CLASS : 
+Name : ANONYMOUS                                                                 
+Age : 17                                                                         
+DERIVED CLASS :                                                                  
+Name : ANGEL                                                                     
+Age : 17                                                                         
+*/ 
