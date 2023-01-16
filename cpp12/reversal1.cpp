fb45d9e6440dbb1cd5e69d3d21a7f787e0537554
ayush13073 <104487018+ayush13073@users.noreply.github.com>
2023-01-16 06:20:31 +0530

    Update reversal1.cpp

--- cpp12/reversal1.cpp
+++ cpp12/reversal1.cpp
@@ -1,33 +1,33 @@
 #include <iostream.h>
 #include <conio.h>
 #include <stdio.h>
-#include <string.h>
+
+void reverse(char a[]) // function defination
+{
+char b[50]; // string declaration
+int i = 0, j = 0; // variable initialisation
+while(a[i] != '\0')
+i++;
+while(--i >= 0)
+{
+b[j++] = a[i];
+b[j] = '\0';
+}
+cout << "The reverse of string is :" << endl << b << endl;
+}
 
 void main()
 {
 clrscr();
-cout << "Enter a string which you want to reverse:" << endl;
-char s[30], temp, i; // variable declaration and array declaration
-gets(s);
-int len = strlen(s);
-int l = len - 1;
-for (i = 0; i < (len/2); i++)
-{
-temp = s[i];
-s[i] = s[l];
-s[l] = temp;
-l--;
-}
-cout << endl << "Reverse of given string:" << endl << s << endl;
+char a[50];
+cout << "Enter a string to be reversed:" << endl;
+gets(a);
+reverse(a); // function call
 getch();
 }
-
 /* Output:
-
-Enter a string which you want to reverse:
-computer science
-
-Reverse of given string:
-ecneics retupmoc
-
+Enter a string to be reversed:
+computers
+The reverse of string is :
+sretupmoc
 */
